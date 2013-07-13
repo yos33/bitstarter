6113452ce19fbfa9b041e0606aab121352d04b59
Yossi <yossihai@hotmail.com>
2013-07-13 23:31:57 +0000

    finally

---
+++ grader5.js
@@ -0,0 +1,178 @@
+#!/usr/bin/env node
+/*
+Automatically grade files for the presence of specified HTML tags/attributes.
+Uses commander.js and cheerio. Teaches command line application development
+and basic DOM parsing.
+
+References:
+
+ + cheerio
+   - https://github.com/MatthewMueller/cheerio
+   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
+   - http://maxogden.com/scraping-with-node.html
+
+ + commander.js
+   - https://github.com/visionmedia/commander.js
+   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
+
+ + JSON
+   - http://en.wikipedia.org/wiki/JSON
+   - https://developer.mozilla.org/en-US/docs/JSON
+   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
+*/
+
+var fs = require('fs');
+var program = require('commander');
+var cheerio = require('cheerio');
+var rest = require('restler');
+
+var HTMLFILE_DEFAULT = "index.html";
+var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "www.googe.com";
+
+var assertFileExists = function(infile) {
+    var instr = infile.toString();
+    if(!fs.existsSync(instr)) {
+        console.log("%s does not exist. Exiting.", instr);
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    }
+    return instr;
+};
+
+var cheerioHtmlFile = function(htmlfile) {
+    return cheerio.load(fs.readFileSync(htmlfile));
+};
+
+var loadChecks = function(checksfile) {
+    return JSON.parse(fs.readFileSync(checksfile));
+};
+
+var getFileFromUrl = function(url) {
+    
+    rest.get(url).on('complete', response2console);
+
+}
+
+var checkHtmlFile = function(htmlfile, checksfile) {
+    $ = cheerioHtmlFile(htmlfile);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
+
+
+
+var aaa_checkURL = function(url, checksfile) {
+var out ={};   
+//rest.get(url.toString()).on('complete', function(result) {
+   rest.get("https://github.com/yos33/bitstarter/blob/master/index.html").on('complete', function(result) {
+        if (result instanceof Error) {
+        sys.puts('Error: ' + result.message);
+        this.retry(5000); // try again after 5 sec
+        } else {       
+	$=cheerio.load(result);
+	var checks = loadChecks(checksfile).sort();
+    	for(var ii in checks) {
+        	var present = $(checks[ii]).length > 0;
+        	out[checks[ii]] = present;
+    	}
+    	return out;
+       }
+    });
+return out;
+}
+
+
+var checkURL = function(url, checksfile) {
+
+    $ = aaa_checkUrl(url);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+}
+
+
+var writeFile = function (apiurl,  progcheck) {
+rest.get(apiurl).on('complete',
+   
+   function(result, response) {
+        if (result instanceof Error) {
+            console.error('Error: ' + util.format(response.message));
+        } else {
+            console.error("Wrote %s", "yossi.hai");
+            fs.writeFileSync("yossi.hai", result);            
+		var checkJson = checkHtmlFile("yossi.hai", progcheck);
+                var outJson = JSON.stringify(checkJson, null, 4);
+		console.log("URL ---- required.main if : \n" + outJson + "\n " + program.url);
+return outJson;
+        }
+    } )
+};
+
+
+
+
+
+
+
+var clone = function(fn) {
+    // Workaround for commander.js issue.
+    // http://stackoverflow.com/a/6772648
+    return fn.bind({});
+};
+
+
+
+if (require.main == module) {
+
+program
+ .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+ .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+ .option('-u, --url <url>', 'path to url')
+  // .option('-u, --url <url>', 'path to herokuapp url', clone(assertURLExists), CHECKSURL_DEFAULT)
+ .parse(process.argv);
+
+if (!program.url) {
+ //original logic. file existing - index.html
+	var checkJson = checkHtmlFile(program.file, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log("File-----required.main if : \n" + outJson);
+	} else { // added // 
+
+	//	var checkJson = aaa_checkURL(program.url, program.checks);
+		writeFile(program.url, program.checks);
+		//var checkJson = checkHtmlFile("yossi.hai", program.checks);
+		//var outJson = JSON.stringify(checkJson, null, 4);
+		//console.log("URL ---- required.main if : \n" + outJson + "\n " + program.url);
+	} 
+}
+else {
+exports.checkHtmlFile = checkHtmlFile;
+}
+
+
+
+
+
+//
+//if(require.main == module) {
+//   program
+//        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+//        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+//	.option('-u, --url<URL>', 'Path to url'); 
+//        .parse(process.argv);
+//    var checkJson = checkHtmlFile(program.file, program.checks);
+//    var outJson = JSON.stringify(checkJson, null, 4);
+//    var 
+//    console.log(outJson);
+//} else {
+//    exports.checkHtmlFile = checkHtmlFile;
+//}
