e424140ec0252053cf7400e0ffd3b475f875d7f9
Yossi <yossihai@hotmail.com>
2013-07-14 00:10:43 +0000

    hw3 part3

--- old-grader.js
+++ old-grader.js
@@ -24,11 +24,8 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
-var rest = require('restler');
-
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-var URL_DEFAULT = "www.googe.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -58,61 +55,20 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
-
-// var checkURL = function(url, checksfile) {
-//
-//    $ = aaa_checkUrl(url);
-//    var checks = loadChecks(checksfile).sort();
-//    var out = {};
-//    for(var ii in checks) {
-//        var present = $(checks[ii]).length > 0;
-//        out[checks[ii]] = present;
-//    }
-//    return out;
-//}
-
-
-var writeFile = function (apiurl,  progcheck) {
-rest.get(apiurl).on('complete',
-   
-   function(result, response) {
-        if (result instanceof Error) {
-            console.error('Error: ' + util.format(response.message));
-        } else {
-            //console.error("Wrote %s", "yossi.tmp");
-             fs.writeFileSync("yossi.tmp", result);            
-		var checkJson = checkHtmlFile("yossi.tmp", progcheck);
-                var outJson = JSON.stringify(checkJson, null, 4);
-		console.log(outJson);
-		return outJson;
-        }
-    } )
-};
-
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
 
-
-if (require.main == module) {
-program
- .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
- .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
- .option('-u, --url <url>', 'path to url')
- .parse(process.argv);
-
-if (!program.url) {
- //file exist
-	var checkJson = checkHtmlFile(program.file, program.checks);
-	var outJson = JSON.stringify(checkJson, null, 4);
-	console.log(outJson);
-	} else { // write tmp file //
-			writeFile(program.url, program.checks);
-		} 
+if(require.main == module) {
+    program
+        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .parse(process.argv);
+    var checkJson = checkHtmlFile(program.file, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+} else {
+    exports.checkHtmlFile = checkHtmlFile;
 }
-else {
-exports.checkHtmlFile = checkHtmlFile;
-}
-
